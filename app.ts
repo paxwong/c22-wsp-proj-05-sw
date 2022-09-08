361a43b588c35421c11b4cf50f0276ecd75b6b56
Ronson Kwok <rk6866@hotmail.com>
2022-09-08 15:08:11 +0800

    Amended Server to basic setup

--- app.ts
+++ app.ts
@@ -1,50 +1,50 @@
 import express from 'express'
-import { userRoutes } from './routes/userRoute'
-import fs from 'fs'
-import { uploadDir } from './utils/upload'
-import { logger } from './utils/logger'
-import { memosRoutes } from './routes/memoRoute'
-import http from 'http'
-import { Server as SocketIO } from 'socket.io'
-import { loggingUserRoute } from './utils/guard'
-import { grantExpress, sessionMiddleware } from './utils/middleware'
-import { setIO } from './utils/setIO'
+// import { userRoutes } from './routes/userRoute'
+// import fs from 'fs'
+// import { uploadDir } from './utils/upload'
+// import { logger } from './utils/logger'
+// import { memosRoutes } from './routes/memoRoute'
+// import http from 'http'
+// import { Server as SocketIO } from 'socket.io'
+// import { loggingUserRoute } from './utils/guard'
+// import { grantExpress, sessionMiddleware } from './utils/middleware'
+// import { setIO } from './utils/setIO'
 
-declare module 'express-session' {
-	interface SessionData {
-		name?: string
-		isloggedin?: boolean
-	}
-}
+// declare module 'express-session' {
+// 	interface SessionData {
+// 		name?: string
+// 		isloggedin?: boolean
+// 	}
+// }
 
 export const app = express()
-const server = new http.Server(app)
-export const io = new SocketIO(server)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(sessionMiddleware)
+// const server = new http.Server(app)
+// export const io = new SocketIO(server)
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(sessionMiddleware)
 
-app.use(grantExpress as express.RequestHandler)
-app.use('/user', loggingUserRoute, userRoutes)
-app.use('/memos', memosRoutes)
-app.get('/test-logger', (req, res) => {
-	logger.error('This is error')
-	logger.warn('This is warn')
-	logger.info('This is info')
-	logger.verbose('This is verbose')
-	logger.debug('This is debug')
-	res.end('ok')
-})
+// app.use(grantExpress as express.RequestHandler)
+// app.use('/user', loggingUserRoute, userRoutes)
+// app.use('/memos', memosRoutes)
+// app.get('/test-logger', (req, res) => {
+// 	logger.error('This is error')
+// 	logger.warn('This is warn')
+// 	logger.info('This is info')
+// 	logger.verbose('This is verbose')
+// 	logger.debug('This is debug')
+// 	res.end('ok')
+// })
 app.use(express.static('public')) // auto to do next()
 
 // GET /use/promotion
-app.use('/uploads', express.static('uploads')) // auto to do next()
+// app.use('/uploads', express.static('uploads')) // auto to do next()
 app.use((req, res) => {
 	res.redirect('/404.html')
 })
-setIO(io)
-server.listen(8080, () => {
+// setIO(io)
+app.listen(8080, () => {
 	// Auto create a folder
-	fs.mkdirSync(uploadDir, { recursive: true })
-	logger.info('Listening on port 8080')
+	// fs.mkdirSync(uploadDir, { recursive: true })
+	console.log('Listening on port 8080')
 })
