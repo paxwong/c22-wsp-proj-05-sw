6dbb9c3cc2ab3079096f646d159353b0c0429da4
Ronson Kwok <rk6866@hotmail.com>
2022-09-09 13:02:17 +0800

    updated env.ts

--- Utils/env.ts
+++ Utils/env.ts
@@ -3,10 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export let env = {
-	DB_NAME: process.env.DB_NAME,
-	DB_USERNAME: process.env.DB_USERNAME,
-	DB_PASSWORD: process.env.DB_PASSWORD,
+	SERVER_PORT: process.env.SERVER_PORT,
+	DB_NAME: process.env.DB_NAME || 'freelance_platform',
+	DB_USERNAME: process.env.DB_USERNAME || "postgres",
+	DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
 	SESSION_SECRET: process.env.SESSION_SECRET || 'default secret',
-	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '',
-	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || ''
+	// GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '',
+	// GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || ''
 }
