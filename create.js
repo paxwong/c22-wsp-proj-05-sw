4ef417713015bbbfce107405c5c0b95cb2c76e41
tszlongip <tszlongip@gmail.com>
2022-09-11 22:35:53 +0800

    create-contract js file

---
+++ create.js
@@ -0,0 +1,13 @@
+// Get data from the form and display it
+async function displayContract() {
+    const res = await fetch("/");
+    const contractData = await res.json();
+
+    let htmlStr = ``;
+    for (const )
+}
+
+
+document.getElementById("contract-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+})
