8e425895986685ac6762ca3a91f5c634ce5b4a0f
Ronson Kwok <rk6866@hotmail.com>
2022-09-13 08:22:29 +0800

    updated middleware.ts

--- Utils/middleware.ts
+++ Utils/middleware.ts
@@ -12,11 +12,12 @@ export const grantExpress = grant.express({
 		origin: 'http://localhost:8080',
 		transport: 'session',
 		state: true
-	},
-	google: {
-		key: env.GOOGLE_CLIENT_ID,
-		secret: env.GOOGLE_CLIENT_SECRET,
-		scope: ['profile', 'email'],
-		callback: '/user/login/google'
 	}
+	// },
+	// google: {
+	// 	key: env.GOOGLE_CLIENT_ID,
+	// 	secret: env.GOOGLE_CLIENT_SECRET,
+	// 	scope: ['profile', 'email'],
+	// 	callback: '/user/login/google'
+	// }
 })
