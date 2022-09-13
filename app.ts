df499c0b4e411dd4030915f848cf0f3848eb1593
Ronson Kwok <rk6866@hotmail.com>
2022-09-13 11:42:45 +0800

    updated app.ts

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
 import express from 'express'
-// import { userRoutes } from './routes/userRoute'
+import { userRoutes } from './routes/userRoute'
 // import fs from 'fs'
 // import { uploadDir } from './utils/upload'
-// import { logger } from './utils/logger'
+import { logger } from './utils/logger'
 // import { memosRoutes } from './routes/memoRoute'
 // import http from 'http'
 // import { Server as SocketIO } from 'socket.io'
 // import { loggingUserRoute } from './utils/guard'
-// import { grantExpress, sessionMiddleware } from './utils/middleware'
+import { grantExpress, sessionMiddleware } from './utils/middleware'
 // import { setIO } from './utils/setIO'
 
-// declare module 'express-session' {
-// 	interface SessionData {
-// 		name?: string
-// 		isloggedin?: boolean
-// 	}
-// }
+declare module 'express-session' {
+	interface SessionData {
+		name?: string
+		isloggedin?: boolean
+	}
+}
 
 export const app = express()
 
@@ -36,20 +36,20 @@ app.get('/login', async (req, res) => {
 })
 // const server = new http.Server(app)
 // export const io = new SocketIO(server)
-// app.use(express.urlencoded({ extended: true }))
-// app.use(sessionMiddleware)
+app.use(express.urlencoded({ extended: true }))
+app.use(sessionMiddleware)
 
 // app.use(grantExpress as express.RequestHandler)
 // app.use('/user', loggingUserRoute, userRoutes)
 // app.use('/memos', memosRoutes)
-// app.get('/test-logger', (req, res) => {
-// 	logger.error('This is error')
-// 	logger.warn('This is warn')
-// 	logger.info('This is info')
-// 	logger.verbose('This is verbose')
-// 	logger.debug('This is debug')
-// 	res.end('ok')
-// })
+app.get('/test-logger', (req, res) => {
+	logger.error('This is error')
+	logger.warn('This is warn')
+	logger.info('This is info')
+	logger.verbose('This is verbose')
+	logger.debug('This is debug')
+	res.end('ok')
+})
 app.use(express.static('public')) // auto to do next()
 
 // GET /use/promotion
