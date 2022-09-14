c86549f567ddd1a61b391a1917c28941c5aa8aa2
Ronson Kwok <rk6866@hotmail.com>
2022-09-14 16:35:33 +0800

    updated setIO

--- Utils/setIO.ts
+++ Utils/setIO.ts
@@ -1,16 +1,18 @@
 import { Server as SocketIO } from 'socket.io'
 import express from 'express'
 import { sessionMiddleware } from './middleware'
+
 let io: SocketIO
 export function setIO(value: SocketIO) {
 	io = value
-	io.use((socket, next) => {
-		let req = socket.request as express.Request
-		let res = req.res!
-		sessionMiddleware(req, res, next as express.NextFunction)
-	})
 	io.on('connection', function (socket) {
+		const req = socket.request as express.Request;
 		console.log('new socket connected: ', socket.id)
+
+		if (req.session && req.session.user){
+			console.log('joining socket room :', req.session!.user!['username'])
+			socket.join(req.session!.user!['username'])
+		}
 
 		// 如果無login ， 就連socket 都無得用
 		// if (!socket.request['session'].user) {
