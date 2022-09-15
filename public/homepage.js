c887e99e6616808c97ffda95f448acbebc11d7dc
tszlongip <tszlongip@gmail.com>
2022-09-15 16:13:49 +0800

    homepage js testing

--- public/homepage.js
+++ public/homepage.js
@@ -9,7 +9,28 @@ async function getData() {
 
     for (let data of datas) {
         html += `
-            <div class="contract"> Target Name : ${data.name} </div>
+            <div class="contract-container">
+                <div class="contract-profile">
+                    <div class="target-picture"></div>
+                </div>
+                <div class="target-details">
+                    <li>
+                        <div class="target-name">Name: ${data.name}</div>
+                    </li>
+                    <li>
+                        <div class="target-age">Age: ${data.age}</div>
+                    </li>
+                    <li>
+                        <div class="target-nationality">Nationality: ${data.nationality}</div>
+                    </li>
+                    <li>
+                        <div class="target-location">Location: ${data.location}</div>
+                    </li>
+                    <li>
+                        <div class="description">Mission description: ${data.description}</div>
+                    </li>
+                </div>
+            </div>
         `
 
     }
