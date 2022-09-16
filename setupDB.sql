82efc016e4b01e10b9f0bd9cdec60b17fc5c3642
Ronson Kwok <rk6866@hotmail.com>
2022-09-16 18:38:21 +0800

    updated database setup

--- setupDB.sql
+++ setupDB.sql
@@ -14,7 +14,17 @@ CREATE TABLE users (
     task_completion INTEGER 
     -- skills_id INTEGER,
 );
-
+CREATE TABLE target_list (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    nationality VARCHAR(255),
+    age INTEGER,
+    company VARCHAR(255),
+    living_district VARCHAR(255),
+    remarks VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW()
+);
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     bounty INTEGER NOT NULL,
@@ -31,37 +41,27 @@ CREATE TABLE orders (
     -- liked INTEGER NOT NULL,
 );
 
-CREATE TABLE target_list (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    nationality VARCHAR(255),
-    age INTEGER,
-    company VARCHAR(255),
-    living_district VARCHAR(255),
-    remarks VARCHAR(255),
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE photos (
-    id SERIAL PRIMARY KEY,
-    target_id INTEGER NOT NULL,
-    photo VARCHAR(255) NOT NULL, 
-    FOREIGN KEY (target_id) REFERENCES target_list(id)
-);
-
-CREATE TABLE referral (
-    id SERIAL PRIMARY KEY,
-    code VARCHAR(255) NOT NULL,
-    killer_id INTEGER NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    FOREIGN KEY (killer_id) REFERENCES users(id)
-);
 
 CREATE TABLE kill_count (
     id SERIAL PRIMARY KEY,
     count INTEGER NOT NULL
 );
+-- CREATE TABLE photos (
+--     id SERIAL PRIMARY KEY,
+--     target_id INTEGER NOT NULL,
+--     photo VARCHAR(255) NOT NULL, 
+--     FOREIGN KEY (target_id) REFERENCES target_list(id)
+-- );
+
+-- CREATE TABLE referral (
+--     id SERIAL PRIMARY KEY,
+--     code VARCHAR(255) NOT NULL,
+--     killer_id INTEGER NOT NULL,
+--     status VARCHAR(255) NOT NULL,
+--     FOREIGN KEY (killer_id) REFERENCES users(id)
+-- );
+
+
 
 -- NOT USING / FOR FUTURE USES
 -- CREATE TABLE skills (
