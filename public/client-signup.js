36912aeb8e6d24dd5ac222053fa4f392e9288a08
tszlongip <tszlongip@gmail.com>
2022-09-17 02:46:41 +0800

    client- signup link with loginsignup

---
+++ public/client-signup.js
@@ -0,0 +1,12 @@
+// swipe
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('back');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
