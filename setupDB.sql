7a98116bd086c18c277a66b616318a8c75b22133
pax <paxtonw97@gmail.com>
2022-09-16 17:25:58 +0800

    amended some tables and cut jor some tables

--- setupDB.sql
+++ setupDB.sql
@@ -9,26 +9,26 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW(),
     account_type VARCHAR(255) NOT NULL,
-    skills_id INTEGER,
-    task_completion INTEGER NOT NULL DEFAULT 0
+    task_completion INTEGER 
+    -- skills_id INTEGER,
 );
 
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     bounty INTEGER NOT NULL,
-    expiration TIMESTAMP NOT NULL,
-    target_id INTEGER NOT NULL,
-    liked INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    status VARCHAR(255) NOT NULL DEFAULT 'pending',
+    updated_at TIMESTAMP DEFAULT NOW(),
+    status VARCHAR(255) DEFAULT 'pending',
     description VARCHAR(255),
     evidence VARCHAR(255),
+    target_id INTEGER NOT NULL,
     client_id INTEGER NOT NULL,
     FOREIGN KEY (target_id) REFERENCES target_list(id),
     FOREIGN KEY (client_id) REFERENCES users(id)
+    -- expiration TIMESTAMP NOT NULL,
+    -- liked INTEGER NOT NULL,
 );
 
 CREATE TABLE target_list (
@@ -40,26 +40,14 @@ CREATE TABLE target_list (
     living_district VARCHAR(255),
     remarks VARCHAR(255),
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE photos (
     id SERIAL PRIMARY KEY,
     target_id INTEGER NOT NULL,
-    photo TIMESTAMP NOT NULL,
+    photo VARCHAR(255) NOT NULL, 
     FOREIGN KEY (target_id) REFERENCES target_list(id)
-);
-
-CREATE TABLE skills (
-    id SERIAL PRIMARY KEY,
-    skills VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE killer_advertisements (
-    id SERIAL PRIMARY KEY,
-    killer_id INTEGER NOT NULL,
-    content TEXT NOT NULL,
-    status VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE referral (
@@ -74,3 +62,17 @@ CREATE TABLE kill_count (
     id SERIAL PRIMARY KEY,
     count INTEGER NOT NULL
 );
+
+-- NOT USING / FOR FUTURE USES
+-- CREATE TABLE skills (
+--     id SERIAL PRIMARY KEY,
+--     skills VARCHAR(255) NOT NULL
+-- );
+
+-- NOT USING / FOR FUTURE USES
+-- CREATE TABLE killer_advertisements (
+--     id SERIAL PRIMARY KEY,
+--     killer_id INTEGER NOT NULL,
+--     content TEXT NOT NULL,
+--     status VARCHAR(255) NOT NULL,
+-- );
